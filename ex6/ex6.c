811727ab28a8d0c9773fa6f57b1d084b9a470e80
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-30 14:59:27 +1000

    finished ex6

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -16,6 +16,12 @@ int main(int argc, char *argv[])
   printf("I have a first name %s.\n", first_name);
   printf("I have a last name %s.\n", last_name);
   printf("My whole name is %s %c. %s.\n", first_name, initial, last_name);
+  //printf("My whole name is %s %c. %s.\n", initial, first_name, last_name); // sweet error
+  printf("Here it is!!!\n");
+  printf(">");
+  printf("");
+  printf("<\n");
+  printf("There it was...\n");
 
   return 0;
 }
