4fbd2fe4d673bc6cb5b963f53f334d2f74b125d8
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-31 13:34:40 +1000

    finished ex7

--- ex7/ex7.c
+++ ex7/ex7.c
@@ -24,7 +24,67 @@ int main(int argc, char *argv[])
   char nul_byte = '\0';
   int care_percentage = bugs * nul_byte;
   printf("Which means you should care %d%%.\n",
-         care_perfentage);
+         care_percentage);
+
+  // experiments of a mad scientist
+  printf("what the hell is that nul byte?\n");
+  printf("with %%s it is: %s\n", nul_byte);
+  printf("with %%c it is: %c\n", nul_byte);
+
+  long really_long = 1234567890L;
+  unsigned long wat_really_long = 1234567890L;
+  printf("yo %ld\n", really_long);
+  printf("yo %ld\n", wat_really_long);
+
+  really_long *= 1000;
+  wat_really_long *= 1000;
+  printf("yo %ld\n", really_long);
+  printf("ya %ld\n", wat_really_long);
+
+  really_long *= 1000;
+  wat_really_long *= 1000;
+  printf("yo %ld\n", really_long);
+  printf("ya %ld\n", wat_really_long);
+
+  really_long *= 1000;
+  wat_really_long *= 1000;
+  printf("yo %ld\n", really_long);
+  printf("ya %ld\n", wat_really_long);
+
+  really_long *= 1000;
+  wat_really_long *= 1000;
+  printf("yo %ld\n", really_long);
+  printf("ya %ld\n", wat_really_long);
+
+  really_long *= 1000;
+  wat_really_long *= 1000;
+  printf("yo %ld\n", really_long);
+  printf("ya %ld\n", wat_really_long);
+
+  really_long *= 1000;
+  wat_really_long *= 1000;
+  printf("yo %ld\n", really_long);
+  printf("ya %ld\n", wat_really_long);
+
+  printf("ok so I'll make a char and an int\n");
+  
+  char derp = 'A';
+  int herp = 2;
+
+  printf("the char: %c\n", derp);
+  printf("the int: %i\n", herp);
+  printf("the char as an int: %i\n", derp);
+  printf("the int as a char: %c\n", herp);
+
+  printf("now i do something INSANE\n");
+
+  char wat = derp * herp;
+  int wot = derp * herp;
+  
+  printf("wat: %c\n", wat);
+  printf("wat: %i\n", wat);
+  printf("wot: %c\n", wot);
+  printf("wot: %i\n", wot);
 
   return 0;
 }
