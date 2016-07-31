c140388b541a7926beb9f1d03f698bb45959817b
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-31 14:10:04 +1000

    finished ex8

--- ex8/ex8.c
+++ ex8/ex8.c
@@ -8,7 +8,10 @@ int main(int argc, char *argv[])
     'P', 'h', 'i', 'l', 'i', 'p',
     ' ', 'L', '.', ' ',
     'C', 'a', 's', 't', 'i', 'g', 'l', 'i', 'o', 'n', 'e', '\0'
+    //'C', 'a', 's', 't', 'i', 'g', 'l', 'i', 'o', 'n', 'e' // sweet error here
   };
+  //int areas[] = {10, 12, 13, 14, 20};
+  //char name[] = "Philip";
 
   // WARNING: On some systems, you may have to change the
   // %ld in this code to a %u since it will use unsigned ints
@@ -19,6 +22,8 @@ int main(int argc, char *argv[])
           sizeof(areas) / sizeof(int));
   printf("The first area is %d, the 2nd %d.\n",
           areas[0], areas[1]);
+          //areas[10], areas[1]);
+          //name[20], areas[1]);
 
   printf("The size of a char: %ld\n", sizeof(char));
   printf("The size of name (char[]): %ld\n",
@@ -33,6 +38,28 @@ int main(int argc, char *argv[])
 
   printf("name=\"%s\" and full_name=\"%s\"\n",
           name, full_name);
+
+  printf("reassign areas[0]\n");
+  printf("before: %d\n", areas[0]);
+
+  areas[0] = 100;
+
+  printf("after: %d\n", areas[0]);
+
+  //printf("out of range assignment: \n");
+  //printf("before: %d\n", areas[12]);
+
+  //areas[12] = 200;
+
+  //printf("after: %d\n", areas[12]);
+  // terrifyingly, the above WORKS
+  // but am I changing some random bits of memory??
+
+  printf("name[1]: %d\n", name[1]);
+  printf("areas[1]: %d\n", areas[1]);
+  areas[1] = name[1];
+  printf("name[1]: %d\n", name[1]);
+  printf("areas[1]: %d\n", areas[1]);
 
   return 0;
 }
