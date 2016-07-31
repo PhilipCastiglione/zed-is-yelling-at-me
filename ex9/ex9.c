32fddaa21b24fac9c238f1c07d541cf32c09592f
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-31 14:52:06 +1000

    finished ex9

--- ex9/ex9.c
+++ ex9/ex9.c
@@ -3,7 +3,11 @@
 int main(int argc, char *argv[])
 {
   int numbers[4] = {0};
+  //int numbers[4] = {1};
+  //int numbers[4] = {'a'};
   char name[4] = {'a'};
+  //char *name = "a\0\0\0"; // sweet error
+  //char *name = "a\0"; // sweet error
 
   // first, print them out raw
   printf("numbers: %d %d %d %d\n",
@@ -21,6 +25,10 @@ int main(int argc, char *argv[])
   numbers[1] = 2;
   numbers[2] = 3;
   numbers[3] = 4;
+  //numbers[0] = 'P';
+  //numbers[1] = 'h';
+  //numbers[2] = 'i';
+  //numbers[3] = '\0';
 
   // setup the name;
   name[0] = 'P';
@@ -34,6 +42,7 @@ int main(int argc, char *argv[])
           numbers[2], numbers[3]);
   
   printf("name each: %c %c %c %c\n",
+  //printf("name each: %d %d %d %d\n",
           name[0], name[1],
           name[2], name[3]);
 
@@ -48,6 +57,14 @@ int main(int argc, char *argv[])
   printf("another each: %c %c %c %c\n",
           another[0], another[1],
           another[2], another[3]);
+
+  // nope
+  //int *wat = {1,2,3}
+  //printf("wat: %d %d %d\n",
+          //wat[0], wat[1], wat[2]);
+  char *derp = "derp";
+  printf("derp? %s\n", derp);
+  //printf(derp); //sort of but not really
 
   return 0;
 }
