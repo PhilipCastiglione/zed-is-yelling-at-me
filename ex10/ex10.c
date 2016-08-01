147a3f199775036c5aaf0649bde99548df6fc6e2
Philip Castiglione <philipcastiglione@gmail.com>
2016-08-01 18:39:52 +1000

    finished ex10

--- ex10/ex10.c
+++ ex10/ex10.c
@@ -8,8 +8,12 @@ int main(int argc, char *argv[])
   // why a I skipping argv[0]?
   for(i = 1; i < argc; i++) {
   //for(i = 0; i < argc; i++) {
+  //for(int i = 1; i < argc; i++) { // yep
+    //int amIAThing = 5;
     printf("arg %d: %s\n", i, argv[i]);
   }
+
+  //printf("well, %d\n", amIAThing); // no, you are not
 
   // let's make or own array of strings
   char *states[] = {
@@ -19,8 +23,21 @@ int main(int argc, char *argv[])
   int num_states = 4;
 
   for(i = 0; i < num_states; i++) {
+  //for(int i = 0; i < num_states; i++) { // yep
     printf("state %d: %s\n", i, states[i]);
   }
+
+  printf("NULL yo: %s\n", NULL);
+
+  argv[1] = states[1];
+
+  printf("states[1]: %s\n", states[1]);
+  printf("argv[1]: %s\n", argv[1]);
+
+  states[2] = argv[2];
+
+  printf("argv[2]: %s\n", argv[2]);
+  printf("states[2]: %s\n", states[2]);
 
   return 0;
 }
---
+++ ex10/script.rb
@@ -0,0 +1,6 @@
+puts `./ex10 #{(['wat'] * 21681).join(' ')}`
+# that's a lot of args
+puts 2 ** 14
+puts 2 ** 15
+# wat
+# that is a strange number
