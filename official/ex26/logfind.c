37dfb4d32801185318d2bfdb05042fd2285d288b
Philip Castiglione <philipcastiglione@gmail.com>
2016-10-29 11:52:08 +1100

    hello world, plus program plan

---
+++ official/ex26/logfind.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, world!\n");
+    
+    // handle args (with -o)
+    // check ~/.logfile for list
+    // permit glob
+
+    // scan the files
+    // output the filename being searched
+    // FIRST PASS: check the file has the words with AND/OR
+    // if false, keep going
+    // as soon as true, bail on FIRST PASS and start...
+    // SECOND PASS: output each line found, decorated
+    // output summary for file (instances, lines)
+    // output overall summary (instances, lines, files)
+}
